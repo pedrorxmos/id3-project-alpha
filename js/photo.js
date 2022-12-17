79daf449a376323ae7280d50583d46208327e5ca
Pedro Ramos <pedroaramosguardiola@gmail.com>
2022-12-17 18:49:18 +0100

    🎨 fix: photo animation improved with scss classes

--- js/photo.js
+++ js/photo.js
@@ -7,15 +7,15 @@ window.addEventListener('scroll', () => {
   // console.log(pageYOffset, photo.clientHeight, photo.offsetHeight, photo.clientHeight + (4 * rem));
 
 	if((pageYOffset < photo.clientHeight + (4 * rem)) || (pageYOffset > (photo.clientHeight * 2)  + (24 * rem))){
-    photoBorder.style.transform = 'translate(0px, 0px)';
-    photoBorder.style.borderColor = '#1a1a1a';
-    photoImg.style.transform = 'translate(0px, 0px)';
-    photoImg.style.filter = 'grayscale(1)';
+    photoBorder.classList.remove('photo__border-in');
+    photoBorder.classList.add('photo__border-out');
+    photoImg.classList.remove('photo__img-in');
+    photoImg.classList.add('photo__img-out');
   }
   else {
-    photoBorder.style.transform = 'translate(10px, 10px)';
-    photoBorder.style.borderColor = '#ba68f4';
-    photoImg.style.transform = 'translate(-20px, -20px)';
-    photoImg.style.filter = 'grayscale(0)';
+    photoBorder.classList.remove('photo__border-out');
+    photoBorder.classList.add('photo__border-in');
+    photoImg.classList.remove('photo__img-out');
+    photoImg.classList.add('photo__img-in');
   }
 });
