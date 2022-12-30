8177b477976feb05627bb115d44c518ca81a9e6e
Pedro Ramos <pedroaramosguardiola@gmail.com>
2022-12-30 21:16:47 +0100

    💄 feat: popup form notification script and style

---
+++ js/form.js
@@ -0,0 +1,12 @@
+﻿const form = document.querySelector('#js-form');
+const mssg = document.querySelector('#js-message');
+
+form.addEventListener('submit', (event) => {
+	const data = new FormData(form);
+	for (const [name, value] of data) {
+		console.log(name, ':', value);
+	}
+	event.preventDefault();
+	form.reset();
+	mssg.style.maxHeight = '100%';
+});
