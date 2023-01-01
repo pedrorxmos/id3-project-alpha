049730d613f8cd93729442f3b2909506eca1a3ef
Pedro Ramos <pedroaramosguardiola@gmail.com>
2023-01-01 21:14:52 +0100

    🔥 fix: code cleaned

--- js/photo.js
+++ js/photo.js
@@ -1,14 +1,8 @@
 ﻿const photo = document.querySelector('#js-photo');
 const photoBorder = document.querySelector('#js-photo-border');
 const photoImg =  document.querySelector('#js-photo-img');
-//rem is declared in navbar.js
-
-console.log(photo.clientHeight)
 
 window.addEventListener('scroll', () => {
-
-  console.log('top', photo.getBoundingClientRect().top);
-  console.log('bottom', photo.getBoundingClientRect().bottom);
 
 	if((photo.getBoundingClientRect().top < window.innerHeight - photo.clientHeight/2) && (photo.getBoundingClientRect().bottom > photo.clientHeight/2)){
     photo.classList.remove('photo-out');
